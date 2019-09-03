ee9d7d5ff798eec190379b85ef954a977996a352
Dario Vladovic <d.vladimyr@gmail.com>
2019-09-04 00:29:44 +0200

    Update ESLint config :wrench:

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,5 @@
 'use strict';
 
-const config = require('@vladimyr/eslint-config');
-const [esmConfig] = config.overrides;
-
-
 module.exports = {
-  extends: '@vladimyr',
-  overrides: [{
-    ...esmConfig,
-    files: ['rollup.config.js']
-  }]
+  extends: '@vladimyr'
 };
